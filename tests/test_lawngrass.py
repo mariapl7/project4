92e47a364d75d76b055be5ee88d3b017100ddf86
miplo <miplotnova@mail.ru>
2024-11-11 19:06:40 +0500

    homework_16_1

--- tests/test_lawngrass.py
+++ tests/test_lawngrass.py
@@ -1,4 +1,4 @@
-def test_lawn_grass_properties(grass1):
+def test_grass_properties(grass1):
     assert grass1.name == "Газонная трава"
     assert grass1.description == "Элитная трава для газона"
     assert grass1.price == 500.0
@@ -8,13 +8,13 @@ def test_lawn_grass_properties(grass1):
     assert grass1.color == "Зеленый"
 
 
-def test_lawn_grass_addition(grass1, grass2):
+def test_grass_addition(grass1, grass2):
     result = grass1 + grass2
     expected_result = (grass1.price * grass1.quantity) + (grass2.price * grass2.quantity)
     assert result == expected_result
 
 
-def test_lawn_grass_addition_invalid_type(grass1):
+def test_grass_addition_invalid_type(grass1):
     try:
         grass1 + "Некорректный продукт"  # Попытка сложить LawnGrass с строкой
     except TypeError as e:
